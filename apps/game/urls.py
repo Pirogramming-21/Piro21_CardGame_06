2fb2b5a3c8269ed40f7bfee8ff7f7ed42850caf6
pyun seona <vustjsdk1@naver.com>
2024-07-19 16:12:11 +0900

    list.html까지 완료

--- apps/game/urls.py
+++ apps/game/urls.py
@@ -4,5 +4,7 @@ from .views import *
 app_name = 'game'
 
 urlpatterns = [
-
+  path('signup/', signup, name='signup'),
+  path('ranking/', ranking, name='ranking'),
+  path('list/', list, name='list'),
 ]
--- apps/game/views.py
+++ apps/game/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import *
 
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def ranking(request):
+    return render(request,'ranking.html')
+
+def list(request):
+    return render(request,'list.html')
